4c1c3123b1383d152ecd9fb136ee9519408d3f7f
kalyan199999 <gottapupavankalyan@gmail.com>
2025-11-19 13:25:07 +0530

    main is updated!

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def predict():
 
 if __name__ == '__main__':
 
-    print( os.listdir(curr_dir) )
+    
     # http://127.0.0.1:5000
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port , debug=True)
